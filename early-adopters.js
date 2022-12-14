0efd233f45f0f82a411d21ea44586ed204813d1c
Fatih Yavuz <fatihjy@gmail.com>
2022-12-14 18:56:52 +0100

    add early adopters

---
+++ early-adopters.js
@@ -0,0 +1,14 @@
+users = [
+    {
+        "name": "Fatih Yavuz",
+        "username": "fatihyavuz",
+        "description": "Software Engineer @Spotify",
+        "image": "https://pbs.twimg.com/profile_images/1593630240961662979/hDHYl9MP_400x400.jpg"
+    },
+    {
+        "name": "Birkan Atıcı",
+        "username": "birkan",
+        "description": "Software Engineer",
+        "image": "https://media-exp1.licdn.com/dms/image/C4D03AQE5ywsv2D-twg/profile-displayphoto-shrink_800_800/0/1622719386755?e=1674691200&v=beta&t=nGp54lHJJL8gw_38rJZY4ty6aNZD7DUSjdIjTKIv7oI"
+    }
+]

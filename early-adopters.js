623f993007716c306c073dffcae68b22bf776be7
kodman <ufuk.kodman@gmail.com>
2022-12-15 15:06:26 +0100

    reserving 'kodman' as username

--- early-adopters.js
+++ early-adopters.js
@@ -1,26 +1,36 @@
 users = [
-    {
-        "name": "Fatih Yavuz",
-        "username": "fatih",
-        "description": "Software Engineer @Spotify",
-        "image": "https://pbs.twimg.com/profile_images/1593630240961662979/hDHYl9MP_400x400.jpg"
-    },
-    {
-        "name": "Sila Eryilmaz",
-        "username": "sila",
-        "description": "Software Engineer @Mollie",
-        "image": "https://pbs.twimg.com/profile_images/1564258152056602626/1yxj4TXo_400x400.jpg"
-    },
-    {
-        "name": "Birkan Atıcı",
-        "username": "birkan",
-        "description": "Software Engineer",
-        "image": "https://media-exp1.licdn.com/dms/image/C4D03AQE5ywsv2D-twg/profile-displayphoto-shrink_800_800/0/1622719386755?e=1674691200&v=beta&t=nGp54lHJJL8gw_38rJZY4ty6aNZD7DUSjdIjTKIv7oI"
-    },
-    {
-        "name": "Ali Yilmaz",
-        "username": "ali",
-        "description": "Software Engineer @Facebook",
-        "image": "https://i.ibb.co/nDX800N/IMG-1901.jpg"
-    }
-]
+  {
+    name: "Fatih Yavuz",
+    username: "fatih",
+    description: "Software Engineer @Spotify",
+    image:
+      "https://pbs.twimg.com/profile_images/1593630240961662979/hDHYl9MP_400x400.jpg",
+  },
+  {
+    name: "Sila Eryilmaz",
+    username: "sila",
+    description: "Software Engineer @Mollie",
+    image:
+      "https://pbs.twimg.com/profile_images/1564258152056602626/1yxj4TXo_400x400.jpg",
+  },
+  {
+    name: "Birkan Atıcı",
+    username: "birkan",
+    description: "Software Engineer",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQE5ywsv2D-twg/profile-displayphoto-shrink_800_800/0/1622719386755?e=1674691200&v=beta&t=nGp54lHJJL8gw_38rJZY4ty6aNZD7DUSjdIjTKIv7oI",
+  },
+  {
+    name: "Ali Yilmaz",
+    username: "ali",
+    description: "Software Engineer @Facebook",
+    image: "https://i.ibb.co/nDX800N/IMG-1901.jpg",
+  },
+  {
+    name: "Ufuk Kodaman",
+    username: "kodman",
+    description: "Software Engineer @Worldline",
+    image:
+      "https://pbs.twimg.com/profile_images/1603390110766178306/ewOd91S2_400x400.jpg",
+  },
+];

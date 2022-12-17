bb6fc410b56fa11d4787ea52886b17c56bd697e1
oguzalb <oguzalba@gmail.com>
2022-12-17 09:28:12 +0100

    Added Oguz Albayrak (#15)

--- early-adopters.js
+++ early-adopters.js
@@ -62,6 +62,12 @@ users = [
       "https://pbs.twimg.com/profile_images/1479946935641509888/HNcrpB9L_400x400.jpg",
   },
   {
+    name: "Oğuz Albayrak",
+    username: "o",
+    description: "Software Developer @Booking",
+    image:
+      "https://pbs.twimg.com/profile_images/1333437534353887232/6zSAlQ14_400x400.jpg",
+  },
     name: "Erhan Büte",
     username: "erhan",
     description: "CTO @moneytolia",

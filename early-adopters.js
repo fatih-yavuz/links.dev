8ffc1c5eee3daee86a7a302964ab00c4d37a6b56
Tolga Gezginiş <tgezginis@gmail.com>
2022-12-17 23:28:15 +0300

    New user: t (#19)

--- early-adopters.js
+++ early-adopters.js
@@ -82,22 +82,28 @@ users = [{
 		"description": "Frontend Developer @SpikyAI & Youtuber",
 		"image": "https://pbs.twimg.com/profile_images/1585152388851273728/VbwyNOR7_400x400.jpg"
 	},
+  {
+    name: "Tolga Gezginiş",
+    username: "t",
+    description: "Engineering Director @SIMPRA",
+    image: "https://pbs.twimg.com/profile_images/1601669168339849217/AeIMm2IY_400x400.jpg"
+  },
 	{
 		"name": "Furkan Kapukaya",
 		"username": "furkan",
 		"description": "Junior Software Developer at Piksel Mutfak",
 		"image": "https://furkankapukayaa.github.io/assets/img/me.jpeg"
-   },
-   {
+  },
+  {
 		"name": "Melih Sivri",
 		"username": "melih",
 		"description": "Backend Engineer @wopehq",
 		"image": "https://pbs.twimg.com/profile_images/1583952674587922433/0gUHF7fq_400x400.jpg"
 	},
   {
-      "name": "Joshua",
-      "username": "j",
-      "description": "Software Engineer & Graphic Designer",
-      "image": "https://go.joshua.cafe/avatar"
+    "name": "Joshua",
+    "username": "j",
+    "description": "Software Engineer & Graphic Designer",
+    "image": "https://go.joshua.cafe/avatar"
   }
 ];

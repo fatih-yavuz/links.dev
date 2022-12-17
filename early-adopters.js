05a6407bf8d399646f7bbc10dabcfa452803232e
Onur <onur.suyalcinkaya@gmail.com>
2022-12-17 01:31:30 +0100

    New user: onur (#13)

--- early-adopters.js
+++ early-adopters.js
@@ -60,5 +60,12 @@ users = [
     description: "Software Developer @Trendyol",
     image:
       "https://pbs.twimg.com/profile_images/1479946935641509888/HNcrpB9L_400x400.jpg",
+  },
+  {
+    name: "Onur Şuyalçınkaya",
+    username: "onur",
+    description: "Frontend Software Engineer @ heycar",
+    image:
+      "https://pbs.twimg.com/profile_images/1601239587719979011/E-kvncKx_400x400.jpg",
   }
 ];

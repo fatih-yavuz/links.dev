5cb8b35e3a4759fcd8a61189732deebf6a50f94c
omercelik <omercelik@users.noreply.github.com>
2022-12-18 21:46:13 +0000

    Update early-adopters.js

--- early-adopters.js
+++ early-adopters.js
@@ -1,109 +1,116 @@
-users = [{
-		"name": "Fatih Yavuz",
-		"username": "fatih",
-		"description": "Software Engineer @Spotify",
-		"image": "https://pbs.twimg.com/profile_images/1593630240961662979/hDHYl9MP_400x400.jpg"
-	},
-	{
-		"name": "Sila Eryilmaz",
-		"username": "sila",
-		"description": "Software Engineer @Mollie",
-		"image": "https://pbs.twimg.com/profile_images/1564258152056602626/1yxj4TXo_400x400.jpg"
-	},
-	{
-		"name": "Ali Goktas",
-		"username": "a",
-		"description": "Software Engineer",
-		"image": "https://media.licdn.com/dms/image/C5603AQHXU-scwq9ZvA/profile-displayphoto-shrink_400_400/0/1590850450625?e=1676505600&v=beta&t=UQ6Mq8rW-D6PHocre_s1Bw9LWHfGvIrkbdlnSJlnooc"
-	},
-	{
-		"name": "Birkan Atıcı",
-		"username": "birkan",
-		"description": "Software Engineer",
-		"image": "https://media-exp1.licdn.com/dms/image/C4D03AQE5ywsv2D-twg/profile-displayphoto-shrink_800_800/0/1622719386755?e=1674691200&v=beta&t=nGp54lHJJL8gw_38rJZY4ty6aNZD7DUSjdIjTKIv7oI"
-	},
-	{
-		"name": "Ali Yilmaz",
-		"username": "ali",
-		"description": "Software Engineer @Facebook",
-		"image": "https://i.ibb.co/nDX800N/IMG-1901.jpg"
-	},
-	{
-		"name": "Ufuk Kodaman",
-		"username": "kodman",
-		"description": "Software Engineer @Worldline",
-		"image": "https://pbs.twimg.com/profile_images/1603390110766178306/ewOd91S2_400x400.jpg"
-	},
-	{
-		"name": "Sezer İltekin",
-		"username": "iltekin",
-		"description": "iOS Developer",
-		"image": "https://pbs.twimg.com/profile_images/1587727782884347909/A-2MfwXM_400x400.jpg"
-	},
-	{
-		"name": "Rıza Sabuncu",
-		"username": "riza",
-		"description": "Offensive Security Engineer @Trendyol",
-		"image": "https://i.imgur.com/HCTwBSn.jpeg"
-	},
-	{
-		"name": "Ahmet Buğra Çakıcı",
-		"username": "ahmet",
-		"description": "Software Developer @Trendyol",
-		"image": "https://pbs.twimg.com/profile_images/1479946935641509888/HNcrpB9L_400x400.jpg"
-	},
-	{
-		"name": "Oğuz Albayrak",
-		"username": "o",
-		"description": "Software Developer @Booking",
-		"image": "https://pbs.twimg.com/profile_images/1333437534353887232/6zSAlQ14_400x400.jpg"
-	},
-	{
-		"name": "Erhan Büte",
-		"username": "erhan",
-		"description": "CTO @moneytolia",
-		"image": "https://pbs.twimg.com/profile_images/1471460414911389702/NrQ94G88_400x400.jpg"
-	},
-	{
-		"name": "Onur Şuyalçınkaya",
-		"username": "onur",
-		"description": "Frontend Software Engineer @ heycar",
-		"image": "https://pbs.twimg.com/profile_images/1601239587719979011/E-kvncKx_400x400.jpg"
-	},
-	{
-		"name": "Zeynep Nur Aktas",
-		"username": "zeynep",
-		"description": "Software Engineer",
-		"image": "https://pbs.twimg.com/profile_images/1581993593018908672/VZ5-XkAe_400x400.jpg"
-	},
-	{
-		"name": "Didem Küçükkaraaslan",
-		"username": "codingwithdidem",
-		"description": "Frontend Developer @SpikyAI & Youtuber",
-		"image": "https://pbs.twimg.com/profile_images/1585152388851273728/VbwyNOR7_400x400.jpg"
-	},
-  {
-    name: "Tolga Gezginiş",
-    username: "t",
-    description: "Engineering Director @SIMPRA",
-    image: "https://pbs.twimg.com/profile_images/1601669168339849217/AeIMm2IY_400x400.jpg"
-  },
-	{
-		"name": "Furkan Kapukaya",
-		"username": "furkan",
-		"description": "Junior Software Developer at Piksel Mutfak",
-		"image": "https://furkankapukayaa.github.io/assets/img/me.jpeg"
-  },
-  {
-		"name": "Melih Sivri",
-		"username": "melih",
-		"description": "Backend Engineer @wopehq",
-		"image": "https://pbs.twimg.com/profile_images/1583952674587922433/0gUHF7fq_400x400.jpg"
-	},
-  {
-    "name": "Joshua",
-    "username": "j",
-    "description": "Software Engineer & Graphic Designer",
-    "image": "https://go.joshua.cafe/avatar"
-  }
+users = [
+    {
+        "name": "Fatih Yavuz",
+        "username": "fatih",
+        "description": "Software Engineer @Spotify",
+        "image": "https://pbs.twimg.com/profile_images/1593630240961662979/hDHYl9MP_400x400.jpg"
+    },
+    {
+        "name": "Sila Eryilmaz",
+        "username": "sila",
+        "description": "Software Engineer @Mollie",
+        "image": "https://pbs.twimg.com/profile_images/1564258152056602626/1yxj4TXo_400x400.jpg"
+    },
+    {
+        "name": "Ali Goktas",
+        "username": "a",
+        "description": "Software Engineer",
+        "image": "https://media.licdn.com/dms/image/C5603AQHXU-scwq9ZvA/profile-displayphoto-shrink_400_400/0/1590850450625?e=1676505600&v=beta&t=UQ6Mq8rW-D6PHocre_s1Bw9LWHfGvIrkbdlnSJlnooc"
+    },
+    {
+        "name": "Birkan Atıcı",
+        "username": "birkan",
+        "description": "Software Engineer",
+        "image": "https://media-exp1.licdn.com/dms/image/C4D03AQE5ywsv2D-twg/profile-displayphoto-shrink_800_800/0/1622719386755?e=1674691200&v=beta&t=nGp54lHJJL8gw_38rJZY4ty6aNZD7DUSjdIjTKIv7oI"
+    },
+    {
+        "name": "Ali Yilmaz",
+        "username": "ali",
+        "description": "Software Engineer @Facebook",
+        "image": "https://i.ibb.co/nDX800N/IMG-1901.jpg"
+    },
+    {
+        "name": "Ufuk Kodaman",
+        "username": "kodman",
+        "description": "Software Engineer @Worldline",
+        "image": "https://pbs.twimg.com/profile_images/1603390110766178306/ewOd91S2_400x400.jpg"
+    },
+    {
+        "name": "Sezer İltekin",
+        "username": "iltekin",
+        "description": "iOS Developer",
+        "image": "https://pbs.twimg.com/profile_images/1587727782884347909/A-2MfwXM_400x400.jpg"
+    },
+    {
+        "name": "Rıza Sabuncu",
+        "username": "riza",
+        "description": "Offensive Security Engineer @Trendyol",
+        "image": "https://i.imgur.com/HCTwBSn.jpeg"
+    },
+    {
+        "name": "Ahmet Buğra Çakıcı",
+        "username": "ahmet",
+        "description": "Software Developer @Trendyol",
+        "image": "https://pbs.twimg.com/profile_images/1479946935641509888/HNcrpB9L_400x400.jpg"
+    },
+    {
+        "name": "Oğuz Albayrak",
+        "username": "o",
+        "description": "Software Developer @Booking",
+        "image": "https://pbs.twimg.com/profile_images/1333437534353887232/6zSAlQ14_400x400.jpg"
+    },
+    {
+        "name": "Erhan Büte",
+        "username": "erhan",
+        "description": "CTO @moneytolia",
+        "image": "https://pbs.twimg.com/profile_images/1471460414911389702/NrQ94G88_400x400.jpg"
+    },
+    {
+        "name": "Onur Şuyalçınkaya",
+        "username": "onur",
+        "description": "Frontend Software Engineer @ heycar",
+        "image": "https://pbs.twimg.com/profile_images/1601239587719979011/E-kvncKx_400x400.jpg"
+    },
+    {
+        "name": "Zeynep Nur Aktas",
+        "username": "zeynep",
+        "description": "Software Engineer",
+        "image": "https://pbs.twimg.com/profile_images/1581993593018908672/VZ5-XkAe_400x400.jpg"
+    },
+    {
+        "name": "Didem Küçükkaraaslan",
+        "username": "codingwithdidem",
+        "description": "Frontend Developer @SpikyAI & Youtuber",
+        "image": "https://pbs.twimg.com/profile_images/1585152388851273728/VbwyNOR7_400x400.jpg"
+    },
+    {
+        "name": "Tolga Gezginiş",
+        "username": "t",
+        "description": "Engineering Director @SIMPRA",
+        "image": "https://pbs.twimg.com/profile_images/1601669168339849217/AeIMm2IY_400x400.jpg"
+    },
+    {
+        "name": "Furkan Kapukaya",
+        "username": "furkan",
+        "description": "Junior Software Developer at Piksel Mutfak",
+        "image": "https://furkankapukayaa.github.io/assets/img/me.jpeg"
+    },
+    {
+        "name": "Melih Sivri",
+        "username": "melih",
+        "description": "Backend Engineer @wopehq",
+        "image": "https://pbs.twimg.com/profile_images/1583952674587922433/0gUHF7fq_400x400.jpg"
+    },
+    {
+        "name": "Joshua",
+        "username": "j",
+        "description": "Software Engineer & Graphic Designer",
+        "image": "https://go.joshua.cafe/avatar"
+    },
+    {
+        "name": "Omer Celik",
+        "username": "omercelik",
+        "description": "Software Engineer",
+        "image": "https://pbs.twimg.com/profile_images/378800000346283682/f615f8f8d510c8e7b5ddac98b886ed7b_400x400.jpeg"
+    }
 ];

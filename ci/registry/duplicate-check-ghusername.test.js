de9294f40a0eed1f23cb6e22784956bf7b13eb6e
Halil İbrahim ceylan <acolops@gmail.com>
2022-12-28 23:48:34 +0300

    duplicate check for github usernames in registry.yaml (#124)

---
+++ ci/registry/duplicate-check-ghusername.test.js
@@ -0,0 +1,21 @@
+const checkDuplicateGithubUserNames = require("./duplicate-check-ghusername");
+
+
+
+test("correct scenario, 2 different github usernames, 2 different domains", async () => {
+    await expect(checkDuplicateGithubUserNames({
+        "alice": {"github_username": "alice-gh"},
+        "bob": {"github_username": "bob-gh"},
+    })).resolves.toBeUndefined() // no error
+});
+
+
+test("wrong scenario, 2 same github username, 2 different domains", async () => {
+    expect.assertions(1);
+    await expect(checkDuplicateGithubUserNames({
+        "alice": {"github_username": "alice-gh"},
+        "bob": {"github_username": "alice-gh"},
+    })).rejects.toThrow('There are duplicated github usernames in the array')
+});
+
+

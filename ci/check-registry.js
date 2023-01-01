3b3880e22964dee3e95dff93da7dc97c1fe5b539
Fatih Yavuz <fatihjy@gmail.com>
2023-01-01 14:10:33 +0100

    add test for ci checks

--- ci/check-registry.js
+++ ci/check-registry.js
@@ -1,100 +1,58 @@
-const productionRegistryUri = 'https://raw.githubusercontent.com/fatih-yavuz/links.dev/main/registry.yaml';
-const shouldCheckOnlyNewUsers = true;
-
 const fs = require('fs');
-const request = require('request');
 const yaml = require('yaml');
 const axios = require('axios');
-const path = require('path');
 const crypto = require('crypto');
 
-const checkDuplicateGithubUserNames = require('./registry/duplicate-check-ghusername')
-// Create a set to store the hashes of the pageJsonContent strings
+const checkDuplicateGithubUserNames = require('./util/duplicate-check-gh-username')
+const getNewUsers = require("./util/get-new-users");
+const downloadRegistry = require("./util/download-registry");
+const getRestrictedUsernames = require('./util/get-restricted-usernames');
+const getRegistry = require('./util/get-registry');
+const {
+    validateGitHubUsernameExistsOnRegistry,
+    validateUsernameIsNotRestricted,
+    validatePage
+} = require('./util/validation');
+const {productionRegistryUri} = require("./util/constant");
 const hashedContents = new Set();
 
+
+function getHash(pageJsonContent) {
+    return crypto.createHash('sha256').update(JSON.stringify(pageJsonContent)).digest('hex');
+}
+
 async function validateRegistry() {
-    // Read the registry.yaml file
-    const isCI = process.env.CI;
-    const registryPath = !isCI ? '../registry.yaml' : 'registry.yaml';
-    const registryFileContent = fs.readFileSync(registryPath, 'utf8');
-    let restrictedUsernamesPath = !isCI ? '../restricted-usernames.yaml' : 'restricted-usernames.yaml';
-    const restrictedUsernamesFileContent = fs.readFileSync(restrictedUsernamesPath, 'utf8');
-    const restrictedUsernames = yaml.parse(restrictedUsernamesFileContent).restricted_usernames;
+    const restrictedUsernames = getRestrictedUsernames();
+    const registry = getRegistry();
 
-    const registry = yaml.parse(registryFileContent);
-    // Validate the structure of the registry
-    if (!registry || !registry.users) {
-        throw new Error('Invalid registry format. The "users" key is missing.');
-    }
-
-    // Verify that each github username has only one prefix/domain 
-    checkDuplicateGithubUserNames(registry.users)
+    await checkDuplicateGithubUserNames(registry.users)
+    registry.users = await getNewUsers(registry.users, await downloadRegistry(productionRegistryUri));
 
 
-    // Get only new users
-    if(shouldCheckOnlyNewUsers) {
-        console.log("shouldCheckOnlyNewUsers flag enabled. Script will check only new users.")
-
-        const productionRegistry = await downloadRegistry();
-
-        const registryUsers = Object.entries(registry.users);
-      
-        const newUsers = Object.fromEntries(
-            registryUsers.filter(function([key]) { 
-                return !productionRegistry.users.hasOwnProperty(key);
-              })
-        );
-        
-        registry.users = newUsers;
-    }
- 
-
-    // Validate each user
     console.log("Validating " + Object.keys(registry.users).length + " users..")
     for (const username in registry.users) {
         const user = registry.users[username];
-        if (!user.github_username) {
-            throw new Error(`Invalid registry format. The user "${username}" is missing the "github_username" field.`);
-        }
+        validateGitHubUsernameExistsOnRegistry(user);
+        validateUsernameIsNotRestricted(restrictedUsernames, user);
 
-        if (restrictedUsernames.includes(user.github_username)) {
-            throw new Error(`The user "${username}" has a restricted username.`);
-        }
-
-        // Make a request to the user's page.json file
         const pageJsonUrl = `https://raw.githubusercontent.com/${user.github_username}/my-links/main/page.json`;
         console.log("Trying to retrieve " + pageJsonUrl);
         const pageJsonResponse = await axios.get(pageJsonUrl);
 
+        const pageJsonContent = validatePage(pageJsonResponse, user);
 
-        if (pageJsonResponse.status !== 200) {
-            throw new Error(`Failed to fetch page.json for user "${username}". HTTP status code: ${pageJsonResponse.status}`);
-        }
-
-        const pageJsonContent = pageJsonResponse.data;
-
-        // Validate the structure of the page.json file
-        if (!pageJsonContent || !pageJsonContent.name || !pageJsonContent.description || !pageJsonContent.image_url || !pageJsonContent.links) {
-            throw new Error(`Invalid page.json format for user "${username}".`);
-        }
-
-        const hash = crypto.createHash('sha256').update(JSON.stringify(pageJsonContent)).digest('hex');
+        const hash = getHash(pageJsonContent);
         if (hashedContents.has(hash)) {
-            console.error('The page.json file for user "' + username + '" is a duplicate ' + pageJsonUrl);
+            console.error(`The page.json file for user "${user}" is a duplicate ${pageJsonUrl}`);
             process.exit(1);
         }
 
         hashedContents.add(hash);
 
 
-        // Validate the image_url field
-        try {
-            const imageUrlResponse = await axios.get(pageJsonContent.image_url, {responseType: 'arraybuffer'});
-            if (imageUrlResponse.status !== 200) {
-                throw new Error(`Invalid image_url for user "${username}". HTTP status code: ${imageUrlResponse.status}`);
-            }
-        } catch (e) {
-            console.warn(`Invalid image_url for user "${username}".`);
+        const imageUrlResponse = await axios.get(pageJsonContent.image_url, {responseType: 'arraybuffer'});
+        if (imageUrlResponse.status !== 200) {
+            throw new Error(`Invalid image_url for user "${username}". HTTP status code: ${imageUrlResponse.status}`);
         }
 
 
@@ -102,18 +60,6 @@ async function validateRegistry() {
     console.log('Registry validation successful.');
 }
 
-
-async function downloadRegistry() {
-    return new Promise((resolve, reject) => {
-      request.get(productionRegistryUri, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var productionRegistryFileContent = body;
-          resolve(yaml.parse(productionRegistryFileContent));
-        }
-        reject('Invalid status code <' + response.statusCode + '>');
-      });
-    });
-  }
 
 async function main() {
     try {

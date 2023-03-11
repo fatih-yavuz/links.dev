e12367ce84534247cefd5bc59b70b1d3deb6fde7
Fatih Yavuz <fatihjy@gmail.com>
2023-03-11 13:42:43 +0100

    remove second server from cache invalidation

--- invalidate-cache.sh
+++ invalidate-cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 1 2; do
+for i in 1; do
   urls=(
     "https://p${i}.links.dev/cdn?path=fatih-yavuz/links.dev/main/landing/css/custom.css?refresh=1"
     "https://p${i}.links.dev/cdn?path=fatih-yavuz/links.dev/main/example-pages.js&refresh=1"
